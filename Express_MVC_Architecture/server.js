78ad99446f3813c1cbedc934907cc14ac9b795c6
sandeep kumar <sss72778895@gmail.com>
2023-11-21 16:53:18 +0530

    commit

--- Express_MVC_Architecture/server.js
+++ Express_MVC_Architecture/server.js
@@ -1,24 +1,27 @@
-const dotenv=require('dotenv');
-dotenv.config({path:'./config.env'});
-const app=require('./app')
-const mongoose=require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const mongoose = require("mongoose");
 
 //Connecting MongoDb with Express.Js
-mongoose.connect(process.env.CONN_STR,{
+mongoose
+  .connect(process.env.CONN_STR, {
     // useNewUrlParser:true
-    }).then((conn)=>{
+  })
+  .then((conn) => {
     console.log(conn);
-    console.log('DB Connection Successful');
-}).catch((error)=>{
-    console.error('Some error has occured '+error);
-});
+    console.log("DB Connection Successful");
+  })
+  .catch((error) => {
+    console.error("Some error has occured " + error);
+  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
 
 //Create Server
 // const port=3000;
-const port=process.env.PORT||3000;
-app.listen(port, ()=>{
-    console.log('Server has started...');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server has started...");
+});
